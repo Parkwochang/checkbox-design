4126bf86c85b3022dd2a9a0a2b7e6eb94264b6d7
DESKTOP-DNEL6IS\bizwiz <water9707@bizwizsystem.co.kr>
2022-11-03 20:04:12 +0900

    progress bar

---
+++ checkbox.js
@@ -0,0 +1,8 @@
+// const bar = document.querySelector('.gauge');
+// const click = document.querySelector('.first');
+// const auto = document.querySelector('.second');
+
+// click.addEventListener('click', (e) => {
+//   // e.preventDefault();
+//   console.log(e.target);
+// });
